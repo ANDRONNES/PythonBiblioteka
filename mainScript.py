e9a424ea132f3c5cba9b9c70d2431f81bfbfae28
andronnes <s30001@pjwstk.edu.pl>
2025-05-25 13:27:05 +0200

    Format mainScript.py

--- mainScript.py
+++ mainScript.py
@@ -4,17 +4,9 @@ from colorama import Fore, Style
 from tabulate import tabulate
 from Services import *
 
-
-# Wojtek, jeszcze zostały metody:
-# Czetelnicy:
-# get_all_reader_history(CztelnikId : int)
-# get_reader_object_by_Id
-
-
-
 print("Witaj w systemie bibliotecznym")
 
-while(True):
+while (True):
     decision = int(input('''Wybierz akcję którą chcesz wykonać: 
 1. Akcje związane z czytelnikiem
 2. Akcje związane z książką
@@ -41,12 +33,12 @@ while(True):
                 case 4:
                     print(tabulate(get_all_readers(), headers='keys', tablefmt='fancy_grid'))
                 case 5:
-                    headers = ["Imię", "Nazwisko", "Tytuł Książki","Operacja","Data Operacji"]
+                    headers = ["Imię", "Nazwisko", "Tytuł Książki", "Operacja", "Data Operacji"]
                     print(tabulate(get_all_reader_history(), headers=headers, tablefmt='fancy_grid'))
                 case 6:
                     continue
                 case _:
-                    print(Fore.RED +"\nWybrano niepoprawną operację!git a\n"+ Style.RESET_ALL)
+                    print(Fore.RED + "\nWybrano niepoprawną operację!git a\n" + Style.RESET_ALL)
                     continue
 
         case 2:
@@ -58,7 +50,7 @@ while(True):
 5. Wróć\n'''))
             match ksiazka_decision:
                 case 1:
-                    nowa_czy_duplikat= int(input('''Chcesz dodać:
+                    nowa_czy_duplikat = int(input('''Chcesz dodać:
 1. Nową książkę
 2. Duplikat
 3. Wróć\n'''))
@@ -70,7 +62,7 @@ while(True):
                         case 3:
                             continue
                         case _:
-                            print(Fore.RED +"\nWybrano niepoprawną operację!\n"+ Style.RESET_ALL)
+                            print(Fore.RED + "\nWybrano niepoprawną operację!\n" + Style.RESET_ALL)
                             continue
 
                 case 2:
@@ -82,7 +74,7 @@ while(True):
                 case 5:
                     continue
                 case _:
-                    print(Fore.RED +"\nWybrano niepoprawną operację!\n"+ Style.RESET_ALL)
+                    print(Fore.RED + "\nWybrano niepoprawną operację!\n" + Style.RESET_ALL)
                     continue
 
         case 3:
@@ -110,7 +102,7 @@ while(True):
                 case 7:
                     continue
                 case _:
-                    print(Fore.RED +"\nWybrano niepoprawną operację!\n"+ Style.RESET_ALL)
+                    print(Fore.RED + "\nWybrano niepoprawną operację!\n" + Style.RESET_ALL)
                     continue
 
         case 4:
@@ -132,58 +124,10 @@ while(True):
                 case 5:
                     continue
                 case _:
-                    print(Fore.RED + "\nWybrano niepoprawną operację!\n"+ Style.RESET_ALL)
+                    print(Fore.RED + "\nWybrano niepoprawną operację!\n" + Style.RESET_ALL)
                     continue
         case 5:
             break
         case _:
-            print(Fore.RED +"\nNie rozpoznano operacji!\n"+ Style.RESET_ALL)
+            print(Fore.RED + "\nNie rozpoznano operacji!\n" + Style.RESET_ALL)
             continue
-
-# add_new_book()
-# booksList = get_all_books()
-# print(tabulate(booksList, headers='keys', tablefmt='fancy_grid'))
-
-# delete_book()
-# edit_book()
-
-#add_new_reader()
-# readers = get_all_readers()
-
-# print(tabulate(readers, headers='keys', tablefmt='fancy_grid'))
-
-# add_new_rent()
-# rents = get_all_rents()
-# print(tabulate(rents, headers='keys', tablefmt='fancy_grid'))
-# delete_rent()
-
-
-#add_new_reservation()
-# print(tabulate(get_all_reservations(), headers='keys', tablefmt='fancy_grid'))
-
-
-# add_new_rent()
-# edit_rent()
-# rents = get_all_rents()
-# print(rents)
-# delete_rent()
-
-# edit_reader()
-
-# add_new_rent()
-# for i in range(5):
-#     addDuplicateBook()
-# add_new_book_prompt()
-# add_new_rent()
-# return_book()
-
-
-
-
-
-# add_new_reader()
-# add_new_reservation()
-# edit_reservation()
-# add_new_rent()
-# przedluzenie_wypozyczenia(3)  #need to add print
-# get_all_reader_history() #need to add imie nazwisko instead of id or a completely different outcome
