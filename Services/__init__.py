4e263f4ea73cf18a50fdf69005f87f8c2d6f530f
s30001 <s30001@pjwstk.edu.pl>
2025-05-28 18:41:32 +0200

    Edited mainScript.py

--- Services/__init__.py
+++ Services/__init__.py
@@ -4,11 +4,11 @@ from .Ksiazki_Services import delete_book, get_all_books, edit_book, add_new_boo
     isBookExists, isBookAvailable
 from .Reservation_service import add_new_reservation, delete_reservation, get_all_reservations, edit_reservation
 from .Wypozyczenia_Services import add_new_rent, delete_rent, get_all_rents, edit_rent, przedluzenie_wypozyczenia, \
-    return_book
+    return_book,validated_input
 
 __all__ = ["add_new_reader", "delete_reader", "get_all_readers", "edit_reader", "get_all_reader_history",
            "get_reader_object_by_Id",
            "delete_book", "get_all_books", "edit_book", "add_new_book_prompt", "addDuplicateBook",
            "add_new_reservation", "delete_reservation", "get_all_reservations", "edit_reservation",
            "add_new_rent", "delete_rent", "get_all_rents", "edit_rent", "przedluzenie_wypozyczenia", "return_book",
-           "isBookExists", "isBookAvailable","isReaderExists"]
+           "isBookExists", "isBookAvailable","isReaderExists","validated_input"]
